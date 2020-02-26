19ae52715ab5a2e21b43926e9c0dd02ae71c08b1
Alin Cocan <alin-ioan.cocan@ing.com>
2020-02-26 17:25:42 +0200

    First commit

---
+++ src/test/java/com/parking/smartcity/SmartCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.smartcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
